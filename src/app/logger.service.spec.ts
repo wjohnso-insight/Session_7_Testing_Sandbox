46816a3087526a7d8e4e49eca53961edd2ea4fd3
Will <will.johnston@insight.com>
2021-02-23 17:42:12 -0500

    Generate Logger service
    1. Generated Logger service from CLI
    2. Added log() method to Logger service
    3. Created "should log input to console" test
    
    This test is carried out against an actual instance of the Logger service and not a stub. Becasue this is a unit test we can test against the actual instance and not a mock.
    
    If we were testing a different unit that has Logger Service as a dependency we would mock the service to avoid pollution.

---
+++ src/app/logger.service.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+
+import { LoggerService } from './logger.service';
+
+describe('LoggerService', () => {
+  let service: LoggerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LoggerService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should log input to console', () => {
+
+    const logger = new LoggerService(); //Calling the actual service and not a stub
+
+    /* 
+      The reason we actually test on a real instance of LoggerService is because we are testing that unit. If we were testing a unit that uses the logger service, 
+      we would mock 'logger', to keep that unit isolated and ensuring that any failures are directly related to that unit and not being polluted by an error in the logger service
+     */
+    
+    spyOn(console, 'log');
+
+    logger.log('Test')
+    expect(console.log).toHaveBeenCalledWith('Test');
+    
+  })
+});
